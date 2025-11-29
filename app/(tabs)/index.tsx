050d43d72ab467bc7a381f5d72ca0b1de8d7d64e
Your Name <you@example.com>
2025-11-29 15:22:05 +0700

    sss

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,8 +16,7 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center', justifyContent: 'center',
   },
   title: {
     fontSize: 20,
